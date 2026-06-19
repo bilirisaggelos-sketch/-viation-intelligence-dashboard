266334410f9b81c35da157ec63e0c05e00ee8db4
bilirisaggelos-sketch <biliris.aggelos@gmail.com>
2026-06-19 22:46:55 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -27,6 +27,26 @@ const countryCoords = {
 };
 
 function formatDate(dateStr) {
+
+    if (!dateStr) return "";
+
+    if (dateStr.includes("/")) {
+
+        const parts = dateStr.split("/");
+
+        const d = new Date(
+            parts[2],
+            parts[1] - 1,
+            parts[0]
+        );
+
+        return d.toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "short",
+            year: "numeric"
+        });
+    }
+
     const d = new Date(dateStr);
 
     return d.toLocaleDateString("en-GB", {
