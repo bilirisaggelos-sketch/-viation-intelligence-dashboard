1986611260a8ee6ae0e7cb7278f32ff2e1a25514
bilirisaggelos-sketch <biliris.aggelos@gmail.com>
2026-06-25 20:00:27 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,29 @@
+// =====================
+// MAP
+// =====================
 
+var map = L.map('map', {
+    zoomControl: false
+}).setView([33,25],4);
+
+const alertIcon = L.divIcon({
+    html:"<div class='alert-marker'>🚨</div>",
+    className:"",
+    iconSize:[50,50]
+});
+
+L.control.zoom({
+    position:'bottomleft'
+}).addTo(map);
+
+L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        maxZoom:18
+    }
+).addTo(map);
+
+let countriesLayer = null;
+let intelMarker = null;
+let airportsLayer = L.layerGroup().addTo(map);
+let airportMarkers = [];
