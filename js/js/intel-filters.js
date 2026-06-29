fd509756db0928b2a27350b828a6768295496eba
bilirisaggelos-sketch <biliris.aggelos@gmail.com>
2026-06-29 15:20:10 +0300

    Update intel-filters.js

--- js/js/intel-filters.js
+++ js/js/intel-filters.js
@@ -1,1 +1,25 @@
+// =====================
+// INTELLIGENCE FILTERS
+// =====================
 
+const aviationKeywords = [
+
+    "missile",
+    "rocket",
+    "drone",
+    "uav",
+    "gps",
+    "gnss",
+    "jam",
+    "jamming",
+    "airspace",
+    "notam",
+    "airport",
+    "runway",
+    "military",
+    "fighter",
+    "surface-to-air",
+    "sam",
+    "closure"
+
+];
